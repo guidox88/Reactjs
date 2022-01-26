8c856e07ca942ad895bc1bae560c9e7e8aeb9e42
Guido Negrisoli <gnegrisoli@gmail.com>
2022-01-26 15:21:00 -0300

    reactclase 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let contador = 1;
+
+  contador++ ;
+  console.log(contador);
+
+  let edad = 18;
+
+  if(edad >= 18) {
+    console.log("es mayor de edad") :
+    {else if{ ( edad > 0 && edad 18)
+    }else{
+    console.log("es menor de edad");
+  }
+  
+  edad >= 18 ? console.log("esmayo de edad") :
+  ( edad > 0 && edad < 18) ? console.log("es menor de edad")
+  : console.log("es menor de edad");
+
+  mensaje = `Juean puede pasar al boliche ya que tiene" ${edad} "y puede acceder a los tragos en algohol`
+  
+  console.log(mensaje);
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
