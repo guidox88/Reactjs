1823920b539b21bc2de8b4fe71d4036cd5b8ee72
Guido Negrisoli <gnegrisoli@gmail.com>
2022-01-31 19:15:13 -0300

    reactclase 2

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,25 @@
 import React from "react";
 
+
 let style = {
-    color:"white",backgroundColor:"Black"
+   
+	margin: "0",padding: "0",overflowY: "auto", overflowX: "hidden",color:"white",backgroundColor:"Black",display: "flex", listStyle: "none",textDecoration:"none",fontSize: "2rem",justifyContent: "center", boxSizing: "border-box"
   }
+  
 
     function NavBar() {
 
-         return <nav style={style}>
-             <ul>Soy ul del navbar</ul> 
-         </nav>;
+         return<ul style = {style} >
+          <li col-ml-3><a href="index.html">Inicio</a></li>
+        
+       <li col-ml-4><a href="Nosotros.html">Nosotros</a></li>
+
+       <li><a href="Productos.html">Productos</a></li>
+
+       <li><a href="Filamentos.html">Filamentos</a></li>
+
+       <li><a href="Contacto.html">Contacto</a></li>
+     </ul>
    
         }
 
